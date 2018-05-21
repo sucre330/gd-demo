092bd85d611e9d0897e570e9f6a5c3c134f469f1
sucre330 <330675022@qq.com>
2018-05-21 14:38:28 +0800

    提交代码

---
+++ src/main/java/com/sucre/stu/Demo.java
@@ -0,0 +1,11 @@
+package com.sucre.stu;
+
+import org.joda.time.LocalTime;
+
+
+public class Demo {
+	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is:"+currentTime);
+	}
+}
